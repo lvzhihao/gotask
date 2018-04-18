db0963f23da979f034a6a05137c2f015020fdbad
edwin <edwin.lzh@gmail.com>
2018-04-18 18:49:24 +0800

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,23 @@ if [ ! -n "$TZ" ]; then
     export TZ="Asia/Shanghai"
 fi
 
+# set timezone
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
-echo $TZ > /etc/timezone && \
-gotask start
+echo $TZ > /etc/timezone 
+
+# k8s config  switch
+if [ -f "/usr/local/gotask/config/.gotask.yaml" ]; then
+    ln -s  /usr/local/gotask/config/.gotask.yaml /usr/local/gotask/.gotask.yaml
+fi
+
+# apply config
+echo "===start==="
+cat /usr/local/gotask/.gotask.yaml
+echo "====end===="
+
+# run command
+if [ ! -z "$1" ]; then
+    /usr/local/gotask/gotask $@
+else
+    /usr/local/gotask/gotask start
+fi
